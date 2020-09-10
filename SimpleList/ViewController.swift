78b027a964771d8c37577f6839a60d0a0dc998d0
Tristan <tristan@audreysmonsters.com>
2020-09-09 20:57:47 -0500

    initial

---
+++ SimpleList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleList
+//
+//  Created by shimanopower on 9/9/20.
+//  Copyright © 2020 shimanopower. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
